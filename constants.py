8bfc302e5194ff6ffa56fa6faf96e2b69be45223
nzachari <nzachari@eecs.wsu.edu>
2019-10-22 17:14:29 -0700

    fixed constants

--- constants.py
+++ constants.py
@@ -10,8 +10,9 @@ USERS = [(USER_1, 1), (USER_2, 2), (USER_3, 3)]
 MOTION_BODY_PARTS = ['Bag_Motion.txt', 'Hand_Motion.txt', 'Hips_Motion.txt', 'Torso_Motion.txt']
 API_BODY_PARTS = ['Bag_API.txt', 'Hand_API.txt', 'Hips_API.txt', 'Torso_API.txt']
 GPS_BODY_PARTS = ['Bag_GPS.txt', 'Hand_GPS.txt', 'Hips_GPS.txt', 'Torso_GPS.txt']
-AMBIENT_BODY_PARTS = ['Bag_API.txt', 'Hand_API.txt', 'Hips_API.txt', 'Torso_API.txt']
-BATTERY_BODY_PARTS = ['Bag_Ambient.txt', 'Hand_Ambient.txt', 'Hips_Ambient.txt', 'Torso_Ambient.txt']
-CELL_BODY_PARTS = ['Bag_Cell.txt', 'Hand_Cell.txt', 'Hips_Cell.txt', 'Torso_Cell.txt']
+BATTERY_BODY_PARTS = ['Bag_Battery.txt', 'Hand_Battery.txt', 'Hips_Battery.txt', 'Torso_Battery.txt']
+AMBIENT_BODY_PARTS = ['Bag_Ambient.txt', 'Hand_Ambient.txt', 'Hips_Ambient.txt', 'Torso_Ambient.txt']
 LOCATION_BODY_PARTS = ['Bag_Location.txt', 'Hand_Location.txt', 'Hips_Location.txt', 'Torso_Location.txt']
+CELL_BODY_PARTS = ['Bag_Cell.txt', 'Hand_Cell.txt', 'Hips_Cell.txt', 'Torso_Cell.txt']
 LABEL = 'Label.txt' #all the other label files like labels_track_food.txt don't seem to have a real purpose
+
